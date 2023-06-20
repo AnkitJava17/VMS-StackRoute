baefadebe62ff4c4ca18c32036121b74e15d3cc2
abhinav-98 <abhinav.goswami@in.ncs-i.com>
2023-06-20 10:18:45 +0530

    added-config-client

--- RentalService/src/main/java/com/rent/RentalService/RentalServiceApplication.java
+++ RentalService/src/main/java/com/rent/RentalService/RentalServiceApplication.java
@@ -2,8 +2,10 @@ package com.rent.RentalService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RentalServiceApplication {
 
 	public static void main(String[] args) {
---
+++ VMS-ConfigServer/src/main/java/com/config/server/VmsConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmsConfigServerApplication.class, args);
+	}
+
+}
