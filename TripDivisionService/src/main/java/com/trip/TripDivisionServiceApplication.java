af34172eb41225da15b8b500ab27475b1db4241e
amit.awasthi <amit.awasthi@in.ncs-i.com>
2023-06-22 23:34:12 +0530

    started trip division service

---
+++ TripDivisionService/src/main/java/com/trip/TripDivisionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class TripDivisionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripDivisionServiceApplication.class, args);
+	}
+
+}
---
+++ TripDivisionService/src/main/java/com/trip/repository/TripRepository.java
@@ -0,0 +1,11 @@
+package com.trip.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.trip.model.TripInfo;
+
+@Repository
+public interface TripRepository extends JpaRepository<TripInfo,Integer> {
+	
+}
